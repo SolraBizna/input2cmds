61a5aee674175634e9b98ffcbd29b7440d199565
Solra Bizna <solra@bizna.name>
2020-07-24 16:53:56 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+This is a simple utility that maps Linux `/dev/input` events to shell commands. Note that this will only work on OSes that support Linux's `/dev/input` interface. **Windows and macOS definitely don't**, though Windows might if you run it through WSL.
+
+Installation
+------------
+
+Recent Rust is required. If you don't have it, you can quickly install a Rust toolchain with this command:
+
+```sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+```
+
+Download the source code (if you haven't already):
+
+```sh
+git clone https://github.com/SolraBizna/input2cmds
+```
+
+Install using Cargo:
+
+```sh
+cargo install --path input2cmds
+```
+
+Quick Start
+-----------
+
+(The below information is also available by running `input2cmds` with no arguments.)
+
+To get started with `input2cmds`, create a configuration file. The file can be named anything you want. Put one or more "dev" directives inside the file, like so:
+
+```
+dev /dev/input/by-id/usb-Gamepad_Name_Goes_Here_USB-event-joystick
+```
+
+Make sure you specify an "event-joystick" device and not a "joystick" device here. Also, be aware that input2cmds doesn't distinguish between input devices (so you can't map the same button on different gamepads to different things, for example).
+
+Once that's done, run input2cmds with the -v option and pass it the path to your configuration file. It will produce output like:
+
+```
+if type=x code=y value=z then: ...
+```
+
+If one of those type/code/value combinations corresponds to a button you want
+to map, then you can paste that line into the configuration file, and replace
+... with the command you want to execute. input2cmds will wait until the
+command has fully executed before executing any further commands (unless you
+put a & on the end).
+
+Example Configuration
+=====================
+
+```
+# Lines beginning with # are comments, which are ignored by input2cmds and
+# exist only to inform the person reading the file.
+
+dev /dev/input/by-id/usb-Gravis_GamePad_Pro_USB-event-joystick
+
+# Media Play/Pause = Green
+if type=1 code=306 value=1 then: xdotool key XF86AudioPlay
+
+# Volume Up = D-Pad Up
+if type=3 code=1 value=0 then: xdotool key XF86AudioRaiseVolume
+# Volume Down = D-Pad Down
+if type=3 code=1 value=255 then: xdotool key XF86AudioLowerVolume
+
+# Keyboard Left = D-Pad Left
+if type=3 code=0 value=0 then: xdotool key Left
+# Keyboard Right = D-Pad Right
+if type=3 code=0 value=255 then: xdotool key Right
+
+# Media Prev = L1
+if type=1 code=308 value=1 then: xdotool key XF86AudioPrev
+# Media Next = R1
+if type=1 code=309 value=1 then: xdotool key XF86AudioNext
+
+# Suspend system = Start
+if type=1 code=313 value=1 then: systemctl -i suspend
+```
+
+License
+-------
+
+This program is distributed under the zlib license. This puts very few restrictions on use. See [`LICENSE.md`](LICENSE.md) for the complete, very short text of the copyright notice and license.
---
+++ src/main.rs
@@ -0,0 +1,329 @@
+//! This is a simple utility that maps Linux `/dev/input` events to shell
+//! commands. See [the README][1] for more information.
+//!
+//! [1]: https://github.com/SolraBizna/input2cmds/blob/master/README.md
+
+use std::{
+    io::{Read, BufRead, BufReader, Error, ErrorKind},
+    process::{exit, Command},
+    sync::mpsc::{channel, Sender},
+    thread::spawn,
+};
+use libc::input_event as InputEvent;
+
+/// Contains a parsed "if ... then ..." line, describing a command to execute
+/// if a certain event is seen.
+#[derive(Clone,Debug,PartialEq,Eq,PartialOrd,Ord)]
+struct InputMatch {
+    /// If not `None`, run this command only if the event type matches this
+    /// value.
+    wants_type: Option<u16>,
+    /// If not `None`, run this command only if the event code matches this
+    /// value.
+    wants_code: Option<u16>,
+    /// If not `None`, run this command only if the event value matches this
+    /// value.
+    wants_value: Option<i32>,
+    /// If all of the above fields matched (or were `None`), run this command
+    /// via `/bin/sh -c command_to_run`.
+    command_to_run: String,
+}
+
+/// Reads a configuration file. For every "dev" directive, opens the given
+/// device and spawns a reader thread that sends events via `event_sender`. For
+/// every "if" directive, adds a match to the `matches` vector.
+fn load_config(path: &str, event_sender: &Sender<InputEvent>,
+               matches: &mut Vec<InputMatch>) -> std::io::Result<()> {
+    let f = std::fs::File::open(path)?;
+    let reader = BufReader::new(f);
+    let mut line_number: usize = 0;
+    for line in reader.lines() {
+        line_number = line_number + 1;
+        let line = line?;
+        let line = line.split('#').next().unwrap_or("");
+        let (line, colon) = if let Some(colon_pos) = line.find(':') {
+            let mut colon = &line[colon_pos+1..];
+            while !colon.is_empty() && colon.chars().next().unwrap()
+                .is_whitespace() {
+                colon = &colon[1..];
+            }
+            (&line[..colon_pos], Some(colon))
+        }
+        else {
+            (line, None)
+        };
+        let mut splat: Vec<&str> = line.split(char::is_whitespace).collect();
+        if let Some(colon) = colon { splat.push(colon) }
+        if splat.is_empty() || splat[0].is_empty() { continue }
+        match splat[0] {
+            "dev" => {
+                if splat.len() != 2 {
+                    return Err(
+                        Error::new(ErrorKind::Other,
+                                   format!("{}:{}: dev wants only one \
+                                            parameter",
+                                           path, line_number,))
+                    )
+                }
+                let event_sender = event_sender.clone();
+                let dev_path = splat[1].to_owned();
+                let dev_file = std::fs::File::open(&dev_path)?;
+                spawn(move || {
+                    let error = format!("Error reading from {:?}", dev_path);
+                    let mut dev_file = BufReader::new(dev_file);
+                    const EVENT_SIZE: usize
+                        = std::mem::size_of::<InputEvent>();
+                    let mut buf = [0u8; EVENT_SIZE];
+                    loop {
+                        dev_file.read_exact(&mut buf[..]).expect(&error);
+                        let event: &InputEvent = unsafe {
+                            std::mem::transmute(&buf)
+                        };
+                        match event.type_ {
+                            0 /* EV_SYN */ | 4 /* EV_MSC */ => continue,
+                            _ => (),
+                        }
+                        if !event_sender.send(*event).is_ok() {
+                            // quietly end the loop, our parent thread is no
+                            // longer listening :(
+                            break
+                        }
+                    }
+                });
+            },
+            "if" => {
+                let mut rest = &splat[1..];
+                let mut wants_type = None;
+                let mut wants_code = None;
+                let mut wants_value = None;
+                while !rest.is_empty() && rest[0] != "then" {
+                    let el = rest[0];
+                    rest = &rest[1..];
+                    if el.starts_with("type=") {
+                        if wants_type.is_some() {
+                            let err = format!("{}:{}: multiple \"type=\"s",
+                                              path, line_number);
+                            return Err(
+                                Error::new(ErrorKind::Other,
+                                                    err)
+                            )
+                        }
+                        let parsed = &el[5..].parse();
+                        match parsed {
+                            Err(_) => {
+                                let err = format!("{}:{}: invalid \"type=\"",
+                                                  path, line_number);
+                                return Err(
+                                    Error::new(ErrorKind::Other,
+                                                        err)
+                                )
+                            },
+                            Ok(x) => {
+                                wants_type = Some(*x);
+                            }
+                        }
+                    }
+                    else if el.starts_with("code=") {
+                        if wants_code.is_some() {
+                            let err = format!("{}:{}: multiple \"code=\"",
+                                              path, line_number);
+                            return Err(
+                                Error::new(ErrorKind::Other,
+                                                    err)
+                            )
+                        }
+                        let parsed = &el[5..].parse();
+                        match parsed {
+                            Err(_) => {
+                                let err = format!("{}:{}: invalid \"code=\"s",
+                                                  path, line_number);
+                                return Err(
+                                    Error::new(ErrorKind::Other,
+                                                        err)
+                                )
+                            },
+                            Ok(x) => {
+                                wants_code = Some(*x);
+                            }
+                        }
+                    }
+                    else if el.starts_with("value=") {
+                        if wants_value.is_some() {
+                            let err = format!("{}:{}: multiple \"value=\"s",
+                                              path, line_number);
+                            return Err(
+                                Error::new(ErrorKind::Other,
+                                                    err)
+                            )
+                        }
+                        let parsed = &el[6..].parse();
+                        match parsed {
+                            Err(_) => {
+                                let err = format!("{}:{}: invalid \"value=\"",
+                                                  path, line_number);
+                                return Err(
+                                    Error::new(ErrorKind::Other,
+                                                        err)
+                                )
+                            },
+                            Ok(x) => {
+                                wants_value = Some(*x);
+                            }
+                        }
+                    }
+                    else {
+                        let err = format!("{}:{}: wanted \"type=\", \
+                                           \"code=\", \"value\"=, or \"then\" \
+                                           after \"if\", saw {:?}",
+                                          path, line_number, el);
+                        return Err(
+                            Error::new(ErrorKind::Other, err)
+                        )
+                    }
+                }
+                rest = &rest[1..]; // skip "then"
+                if rest.is_empty() {
+                    return Err(
+                        Error::new(ErrorKind::Other,
+                                   format!("{}:{}: \"if\" needs a \"then\"",
+                                           path, line_number,))
+                    )
+                }
+                else if rest.len() >= 2 {
+                    return Err(
+                        Error::new(ErrorKind::Other,
+                                   format!("{}:{}: put a colon after \"then\"",
+                                           path, line_number,))
+                    )
+                }
+                matches.push(InputMatch {
+                    wants_type, wants_code, wants_value,
+                    command_to_run: rest[0].to_owned()
+                })
+            },
+            x => {
+                return Err(
+                    Error::new(ErrorKind::Other,
+                               format!("{}:{}: Unknown config directive {:?}",
+                                       path, line_number, x))
+                )
+            },
+        }
+    }
+    // All done!
+    Ok(())
+}
+
+/// Prints a usage string.
+fn print_usage(program_name: &str, opts: getopts::Options) {
+    let brief = format!("Usage: {} [OPTIONS] path/to/config_file.conf \
+                         [ ... more configs ... ]", program_name);
+    print!("{}", opts.usage(&brief));
+}
+
+/// The main function of the program. Parses the command line, calls
+/// [`load_config`](fn.load_config.html) as needed, and then loops reading
+/// events and attempting to match them.
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let program_name = args[0].clone();
+    let mut opts = getopts::Options::new();
+    opts.optflag("h", "help", "");
+    opts.optflag("?", "usage", "Print this help.");
+    opts.optflag("v", "verbose", "Print out all received events, and the \
+                                  commands that they execute (great for if \
+                                  you're still editing your configuration)");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(x) => x,
+        Err(x) => {
+            eprintln!("Error parsing command line: {}", x);
+            print_usage(&program_name, opts);
+            exit(1)
+        },
+    };
+    if matches.opt_present("?") || matches.opt_present("h") {
+        print_usage(&program_name, opts);
+        exit(0);
+    }
+    let verbose = matches.opt_present("v");
+    let free = matches.free;
+    if free.is_empty() {
+        print!(r#"
+To get started with input2cmds, create a configuration file. The file can be
+named anything you want. Put one or more "dev" directives inside the file,
+like so:
+
+dev /dev/input/by-id/usb-Gamepad_Name_Goes_Here_USB-event-joystick
+
+Make sure you specify an "event-joystick" device and not a "joystick" device
+here. Also, be aware that input2cmds doesn't distinguish between input devices
+(so you can't map the same button on different gamepads to different things,
+for example).
+
+Once that's done, run input2cmds with the -v option and pass it the path to
+your configuration file. It will produce output like:
+
+if type=x code=y value=z then: ...
+
+If one of those type/code/value combinations corresponds to a button you want
+to map, then you can paste that line into the configuration file, and replace
+... with the command you want to execute. input2cmds will wait until the
+command has fully executed before executing any further commands (unless you
+put a & on the end).
+"#);
+        exit(0)
+    }
+    let (event_tx, event_rx) = channel();
+    let mut matches = Vec::new();
+    for conf in free.into_iter() {
+        if let Err(x) = load_config(&conf, &event_tx, &mut matches) {
+            eprintln!("Error loading configuration file {:?}: {}", conf, x);
+            exit(1);
+        }
+    }
+    std::mem::drop(event_tx); // we've cloned this poor thing enough
+    while let Ok(event) = event_rx.recv() {
+        let mut command = None;
+        for possibility in matches.iter() {
+            match possibility.wants_type {
+                Some(x) if event.type_ != x => continue,
+                _ => (),
+            }
+            match possibility.wants_code {
+                Some(x) if event.code != x => continue,
+                _ => (),
+            }
+            match possibility.wants_value {
+                Some(x) if event.value != x => continue,
+                _ => (),
+            }
+            command = Some(possibility.command_to_run.as_str());
+            break
+        }
+        match command {
+            Some(command) => {
+                if verbose {
+                    print!("if type={} code={} value={} then: {}",
+                           event.type_, event.code, event.value, command);
+                }
+                let mut child = Command::new("/bin/sh").arg("-c").arg(command)
+                    .spawn().expect("Couldn't execute /bin/sh");
+                let exit_status = child.wait()
+                    .expect("Couldn't wait on child process (?!!)");
+                if exit_status.success() {
+                    println!(" # OK");
+                }
+                else {
+                    println!(" # {}", exit_status);
+                }
+            },
+            None => {
+                if verbose {
+                    println!("if type={} code={} value={} then: ...",
+                             event.type_, event.code, event.value);
+                }
+            }
+        }
+    }
+    std::process::exit(1)
+}
